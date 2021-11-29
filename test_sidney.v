e81c468f1bf14b9a9ce36b74dc69b0c79b5d36a8
Sidney Congard <sidney.congard@gmail.com>
2021-11-29 12:09:37 +0100

    notation!

--- test_sidney.v
+++ test_sidney.v
@@ -2,24 +2,24 @@
 Require Import Nat List Equality.
 Import ListNotations.
 
-Reserved Infix "~>" (at level 90, right associativity).
-Reserved Infix ">>" (at level 40, left associativity).
-
 (* Comment conserver les notations en dehors de la définition ?
    Utilise les univers polymorphiques pour permettre d'avoir une catégorie de catégories.
 *)
+Reserved Notation "a ~> b" (at level 90, right associativity).
+Reserved Notation "f >> g" (at level 40, left associativity).
+
 Polymorphic Class Category := {
     ob   : Type ;
-    hom  : ob -> ob -> Type where "a ~> b" := (hom a b) ;
-    id   {a: ob} : a~>a;
-    comp {a b c: ob} : a~>b -> b~>c -> a~>c
-                where "f >> g" := (comp f g) ;
+    hom  : ob -> ob -> Type where "a ~> b" := (hom a b);
+    id   {a: ob} : a~>a ;
+    comp {a b c: ob} : a~>b -> b~>c -> a~>c where "f >> g" := (comp f g);
 
-    id_r {a b: ob} (f: a~>b) : f  >> id = f ;
-    id_l {a b: ob} (f: a~>b) : id >> f  = f ;
-    assoc {a b c d: ob} (f: a~>b) (g: b~>c) (h: c~>d) :
-        (f >> g) >> h = f >> (g >> h) ;
+    cat_id_r {a b: ob} (f: a~>b) : f  >> id = f ;
+    cat_id_l {a b: ob} (f: a~>b) : id >> f  = f ;
+    cat_comp_assoc {a b c d: ob} (f: a~>b) (g: b~>c) (h: c~>d) : (f >> g) >> h = f >> (g >> h) ;
 }.
+Notation "a ~> b" := (hom a b).
+Notation "f >> g" := (comp f g).
 
 Instance discrete_cat (T: Type) : Category.
 apply (Build_Category T eq (@eq_refl T) (@eq_trans T)).
@@ -28,12 +28,11 @@ apply (Build_Category T eq (@eq_refl T) (@eq_trans T)).
 - intros. apply (eq_sym (eq_trans_assoc f g h)).
 Defined.
 
-(* Les notations me manquent ... *)
 Class Functor (C D: Category) := {
     f_ob : (@ob C) -> (@ob D) ;
-    f_hom {a b: @ob C} (f: @hom C a b) : @hom D (f_ob a) (f_ob b) ;
-    f_commute {a b c: @ob C} (f: @hom C a b) (g: @hom C b c) :
-        f_hom (comp f g) = comp (f_hom f) (f_hom g) ;
+    f_hom {a b: @ob C} (f: a ~> b) : f_ob a ~> f_ob b ;
+    f_commute {a b c: @ob C} (f: a ~> b) (g: b ~> c) :
+        f_hom (f >> g) = (f_hom f) >> (f_hom g) ;
 }.
 
 Instance discrete_functor {A B} (f: A -> B) : Functor (discrete_cat A) (discrete_cat B).
@@ -43,7 +42,7 @@ Defined.
 
 Instance constant_functor (C D: Category) (d: @ob D) : Functor C D.
 apply (Build_Functor C D (fun _ => d) (fun _ _ _ => @id _ d)).
-intros. apply (eq_sym (id_r (@id _ d))).
+intros. apply (eq_sym (cat_id_r (@id _ d))).
 Defined.
 
 Instance id_functor (C: Category) : Functor C C.
@@ -55,24 +54,26 @@ Definition comp_functor {C D E: Category} (F: Functor C D) (G: Functor D E) : Fu
 apply (Build_Functor C E (fun c => f_ob (f_ob c)) (fun _ _ f => f_hom (f_hom f))).
 intros. rewrite <-!f_commute. reflexivity.
 Defined.
+Notation "f >>> g" := (comp_functor f g) (at level 40, left associativity).
 
-Lemma functor_id_r {C D: Category} (F: Functor C D) : comp_functor F (id_functor D) = F.
+Lemma functor_id_r {C D: Category} (F: Functor C D) : F >>> id_functor D = F.
 Proof.
 unfold comp_functor, id_functor. destruct F. simpl. f_equal.
 Admitted.
 
-Lemma functor_id_l {C D: Category} (F: Functor C D) : comp_functor (id_functor C) F = F.
+Lemma functor_id_l {C D: Category} (F: Functor C D) : id_functor C >>> F = F.
 Proof.
 unfold comp_functor, id_functor. destruct F. simpl. f_equal.
 Admitted.
 
 Lemma functor_comp_assoc {A B C D: Category} (F: Functor A B) (G: Functor B C) (H: Functor C D) :
-    comp_functor (comp_functor F G) H = comp_functor F (comp_functor G H).
+    (F >>> G) >>> H = F >>> (G >>> H).
 Proof.
+unfold comp_functor, id_functor. simpl. f_equal.
 Admitted.
 
 Instance category_cat : Category.
-apply (Build_Category Category Functor id_functor (fun _ _ _ f g => comp_functor f g)) ; intros.
+apply (Build_Category Category Functor id_functor (fun _ _ _ f g => f >>> g)) ; intros.
 - apply functor_id_r.
 - apply functor_id_l.
 - apply functor_comp_assoc.
@@ -80,22 +81,22 @@ Admitted.
 
 (* Dégueu *)
 Class Natural_Transformation {C D: Category} (F G: Functor C D) := {
-    n_ob (a: @ob C) : @hom D (@f_ob _ _ F a) (@f_ob _ _ G a);
-    n_commute {a b: @ob C} (f: @hom C a b) :
+    n_ob (a: @ob C) : (@f_ob _ _ F a) ~> (@f_ob _ _ G a);
+    n_commute {a b: @ob C} (f: a ~> b) :
         let Ff := (@f_hom _ _ F _ _ f) in
         let Gf := (@f_hom _ _ G _ _ f) in
-        comp Ff (n_ob b) = comp (n_ob a) Gf ;
+        Ff >> n_ob b = n_ob a >> Gf ;
 }.
 
 Instance id_nat_tr {C D: Category} (F: Functor C D) : Natural_Transformation F F.
 apply (Build_Natural_Transformation _ _ _ _ (fun a => @id _ (f_ob a))).
 intros.
-rewrite id_r, id_l. reflexivity.
+rewrite cat_id_r, cat_id_l. reflexivity.
 Defined.
 
 Instance comp_nat_tr {C D: Category} {F G H: Functor C D}
     (S: Natural_Transformation F G) (T: Natural_Transformation G H) : Natural_Transformation F H.
-apply (Build_Natural_Transformation _ _ _ _ (fun a => comp (n_ob a) (n_ob a) (*TODO: pas la bonne fonction*))).
+apply (Build_Natural_Transformation _ _ _ _ (fun a => n_ob a >> n_ob a (*TODO: pas la bonne fonction*))).
 Admitted.
 
 Instance functor_cat (C D: Category) : Category.
