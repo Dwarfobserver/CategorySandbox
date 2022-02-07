ff7cf824c1ef6d936d60e0922ca83c74923d537b
mchana <mathieu.chanavat@gmail.com>
2022-02-07 19:42:03 +0100

    Yonade, refine error

--- src/Yoneda.v
+++ src/Yoneda.v
@@ -3,28 +3,28 @@ Require Import FunctionalExtensionality.
 
 Set Universe Polymorphism.
 
-Definition hom_ (X Y : Set) := X -> Y.
-Definition id_ (X : Set) := fun x : X => x.
-Definition comp_ {X Y Z : Set} (f : hom_ X Y) (g : hom_ Y Z) := fun (x : X) => g (f x).
+Definition hom_ (X Y : Type) := X -> Y.
+Definition id_ (X : Type) := fun x : X => x.
+Definition comp_ {X Y Z : Type} (f : hom_ X Y) (g : hom_ Y Z) := fun (x : X) => g (f x).
 
-Lemma id_r {X Y : Set} (f : hom_ X Y) : comp_ f (id_ Y) = f.
+Lemma id_r {X Y : Type} (f : hom_ X Y) : comp_ f (id_ Y) = f.
 Proof.
 apply functional_extensionality. intro. now unfold comp_, id_.
 Qed.
 
-Lemma id_l {X Y : Set} (f : hom_ X Y) : comp_ (id_ X) f = f.
+Lemma id_l {X Y : Type} (f : hom_ X Y) : comp_ (id_ X) f = f.
 Proof.
 apply functional_extensionality. intro. now unfold comp_, id_.
 Qed.
 
-Lemma comp_assoc {X Y Z T: Set} (f : hom_ X Y) (g : hom_ Y Z) (h : hom_ Z T) : 
+Lemma comp_assoc {X Y Z T: Type} (f : hom_ X Y) (g : hom_ Y Z) (h : hom_ Z T) : 
     comp_ (comp_ f g) h = comp_ f (comp_ g h).
 Proof.
 apply functional_extensionality. intro. now unfold comp_.
 Qed.
 
 Definition Ens : Category := {|
-    ob := Set;
+    ob := Type;
     hom := hom_;  
     
     id := id_;
@@ -34,12 +34,9 @@ Definition Ens : Category := {|
     cat_id_l := @id_l;
     cat_comp_assoc := @comp_assoc;
 |}.
-(*
-Definition hom_cov {C : Category} (a : [C]) : C → Ens.
 
-
-x:= {|
-    f_ob := fun x => a ~> x ;
+Definition hom_obj {C : Category} (a : [C]) : [C] -> [Ens] := fun b => a ~> b.  
+Program Definition hom_cov {C : Category} (a : [C]) : Functor C Ens := {|
+    f_ob b := hom_obj a b;
     f_hom {a b: [C]} (f: a ~> b) := f_ob a ~> f_ob b ;
 |}.
-*)
